a59e059c8339ebbead663ec59eb24ea717b4707a
tallan groberg <tallan.taven@gmail.com>
2019-11-30 23:13:11 -0700

    basic auth

--- components/SignUp.js
+++ components/SignUp.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, Button, Text, TextInput} from 'react-native'
+import {View, Button, Text, TextInput, AsyncStorage} from 'react-native'
 import {withFirebase} from '../config/firebase/context'
 import {FormLabel} from 'react-native-elements'
 
@@ -8,22 +8,35 @@ import {FormLabel} from 'react-native-elements'
 const SignUp = (props) => {
   const initState = { email: '', password: ''}
   const [inputs, setInputs] = useState(initState)
+  const [response, setResponse] = useState(null)
+  const [errors, setErrors] = useState('')
   // console.log('props in signup',props.firebaseAuth)
-  console.log(inputs)
+  console.log(response)
 
-  const handleChange = () => {
-    setInputs(input => ({...input, email: inputs.email}))
-    
+  const handleSignUp = async () => {
+    try {
+      await props.firebaseAuth.signupWithEmail(inputs.email, inputs.password)
+      .then(res => {
+         props.setUser(res.user)
+      })
+    }
+    catch(err) {
+      setErrors(err.message)
+      console.log(errors)
+    }
   }
 
   return (
     <View>
+      <Text>Sign up. must provide a valid email address.</Text>
       <TextInput placeholder="email"
        onChangeText={ email => setInputs(prev => ({...prev, email}))} 
        value={inputs.email} />
       <TextInput placeholder="password"
        onChangeText={ password => setInputs(prev => ({...prev, password}))}
        value={inputs.password} />
+       <Button title="submit" onPress={handleSignUp} />
+       {errors === '' ? null : <Text>{errors}</Text>}
       
     </View>
       
--- config/firebase/context.js
+++ config/firebase/context.js
@@ -1,16 +1,28 @@
-import React, {createContext} from 'react'
+import React, {createContext, useState} from 'react'
 import {firebaseAuth} from './firebase'
+import {AsyncStorage} from 'react-native'
+
+
 const firebaseContext = createContext({})
 
 const {Provider, Consumer} = firebaseContext
 
 
 const FireBaseProvider = (props) => {
+  const [user, setUser] = useState({})
 
+  const userTokenToAsyncStorage = () => {
+    if(user !== {} ) {
+      AsyncStorage.setItem('token', JSON.stringify(user.uid))
+      console.log(AsyncStorage.getItem('token'))
+    }
+  }
 
   return (
     <Provider value={{
-      firebaseAuth
+      firebaseAuth,
+      user,
+      setUser: setUser,
     }}>
       {props.children}
     </Provider>
